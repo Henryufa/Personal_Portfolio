b93a0243ae32a78e99618569de3c92d7b6b21966
Henryufa <henrywbahr@gmail.com>
2021-09-04 15:35:54 -0500

    First draft

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,11 +19,12 @@ export const heroData = {
 export const aboutData = {
   img: 'red_sweater.jpg',
   paragraphOne:
-    'I am a future mechanical engineer with field and coding experience seeking an internship that will push me to grow as a software developer or mechanical engineer capable of implementing complex solutions like machine learning to solve modern problems and launch polished products fit for industrial use and commercial consumption.',
+    'I am a future mechanical engineer with field and coding experience. I am seeking an internship that will push me to grow as a software developer and/or mechanical engineer. This experience should push me to implement complex solutions like machine learning to solve modern problems and launch polished products fit for industrial use and commercial consumption.',
   paragraphTwo:
     'I expect to finish a bachelors degree in mechanical engineering with minors in math and computer science December of 2022, then begin my masters in computer science.',
   paragraphThree: 'I enjoy disc golf, weight lifting, and challenging myself to learn new things.',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume:
+    'https://docs.google.com/document/d/1HmeZhDqHRiy8VtSGLjpfwkToRUDCcf1rLUqDUnBXo6Y/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -31,11 +32,10 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'Elite_Octane.jpg',
-    title: 'Interned at Elite Octane',
-    info: 'I interned at Elite Octane as a Lab Technician and Plant Operator from June-August, 2021. While I was there, I developed a systematic method for converting spreadsheet data to formatted tests in Microsoft word.',
-    info2:
-      'I also analyzed free amino nitrogen content at eight points throughout 62-hour fermentations and executed 5 independent experiments using small scale fermentation to determine the effect of pH, brand of yeast, and acidity on ethanol yield in fermentation to identify potential yield improvements.',
-    url: '',
+    title: 'Internship at Elite Octane',
+    info: 'I interned at Elite Octane as a Lab Technician and Plant Operator from June-August, 2021. While I was there I: Developed a  systematic method for converting spreadsheet data to formatted employee knowledge tests in Microsoft Word, executed 5 independent experiments using small scale fermentation to improve ethanol yields, and managed the energy production center independently adjusting parameters to correct product composition as needed.',
+    info2: '',
+    url: 'https://www.eliteoctane.net/',
     // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
@@ -43,29 +43,31 @@ export const projectsData = [
     img: 'Mizzou.jpg',
     title: 'School of Study',
     info: 'I am attending the University of Missouri in Columbia, and have a GPA of 3.944. I am taking mechanical engineering, computer science, and math courses and plan to begin computer science research next semester.',
-    info2: '',
-    url: '',
+    info2:
+      'Upon graduating I will begin my masters degree in computer science. I expect to finish my masters December 2023, and I am excited to perform research in machine learning and high performance computing.',
+    url: 'https://missouri.edu/',
     // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
-  {
+  /* {
     id: nanoid(),
     img: 'Gomer_and_me.jpg',
-    title: 'A man and his dog',
+    title: 'Coding',
     info: 'When I am not developing myself I am probably hanging out with my 100 pound hound named Gomer.',
     info2: '',
     url: '',
     // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+  }, */
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: 'If you have a need for advanced engineering talent, leave your name, number, and email here',
-  btn: '',
-  email: '',
+  cta: 'For advanced engineering talent...',
+  btn: 'Send me an email',
+  email: 'hwbfd6@umsystem.edu',
 };
 
 // FOOTER DATA
+/*
 export const footerData = {
   networks: [
     {
@@ -90,6 +92,7 @@ export const footerData = {
     },
   ],
 };
+*/
 
 // Github start/fork buttons
 export const githubButtons = {
---
+++ src/pages/resume.js
@@ -0,0 +1,2 @@
+// I will use the established template to create a good looking page for viewing my resume
+// This will require me to learn to render a word doc with javascript, css, etc.
