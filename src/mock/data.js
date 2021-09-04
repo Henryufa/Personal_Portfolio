6345a1294e14a284928a36904352a5af49bf0475
Henryufa <henrywbahr@gmail.com>
2021-09-04 14:34:47 -0500

    posting version 1.0

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,18 +9,20 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is',
+  name: 'Henry Bahr',
+  subtitle: 'I am the best candidate for your internship',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'red_sweater.jpg',
+  paragraphOne:
+    'I am a future mechanical engineer with field and coding experience seeking an internship that will push me to grow as a software developer or mechanical engineer capable of implementing complex solutions like machine learning to solve modern problems and launch polished products fit for industrial use and commercial consumption.',
+  paragraphTwo:
+    'I expect to finish a bachelors degree in mechanical engineering with minors in math and computer science December of 2022, then begin my masters in computer science.',
+  paragraphThree: 'I enjoy disc golf, weight lifting, and challenging myself to learn new things.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,36 +30,37 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'Elite_Octane.jpg',
+    title: 'Interned at Elite Octane',
+    info: 'I interned at Elite Octane as a Lab Technician and Plant Operator from June-August, 2021. While I was there, I developed a systematic method for converting spreadsheet data to formatted tests in Microsoft word.',
+    info2:
+      'I also analyzed free amino nitrogen content at eight points throughout 62-hour fermentations and executed 5 independent experiments using small scale fermentation to determine the effect of pH, brand of yeast, and acidity on ethanol yield in fermentation to identify potential yield improvements.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'Mizzou.jpg',
+    title: 'School of Study',
+    info: 'I am attending the University of Missouri in Columbia, and have a GPA of 3.944. I am taking mechanical engineering, computer science, and math courses and plan to begin computer science research next semester.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'Gomer_and_me.jpg',
+    title: 'A man and his dog',
+    info: 'When I am not developing myself I am probably hanging out with my 100 pound hound named Gomer.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
+  cta: 'If you have a need for advanced engineering talent, leave your name, number, and email here',
   btn: '',
   email: '',
 };
@@ -90,5 +93,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
